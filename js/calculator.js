a809af21d8ccd190a8c94ae5fe378f4bb9d3d648
Rahat Al Amin <46551614+Rahat33@users.noreply.github.com>
2021-09-15 13:58:00 +0600

    simple calculator done

---
+++ js/calculator.js
@@ -0,0 +1,48 @@
+// document.getElementById('clear-btn').addEventListener('click', function () {
+//     const displayValue = document.getElementById('display-value');
+//     const displayText = displayValue.innerText;
+//     const displayAmount = parseFloat(displayText);
+
+//     displayValue.value = "";
+//     return displayAmount;
+// })
+
+// function displaySum() {
+//     const displayValue = document.getElementById('display-value');
+//     const displayText = displayValue.innerText;
+//     const displayAmount = parseFloat(displayText);
+//     // return displayAmount;
+//     console.log(displayAmount);
+// }
+
+// function addition(num1, num2) {
+//     const display = displaySum();
+//     const add = num1 + num2;
+//     display = add.value;
+// }
+// function subtraction(num1, num2) {
+//     const display = displaySum();
+//     const subtract = num1 - num2;
+//     display = subtract.value;
+// }
+// function multiplication(num1, num2) {
+//     const display = displaySum();
+//     const multiply = num1 * num2;
+//     display = multiply.value;
+// }
+// function division(num1, num2) {
+//     const display = displaySum();
+//     const div = num1 / num2;
+//     display = div.value;
+// }
+
+
+// // document.querySelectorAll('.number').addEventListener('click', function () {
+// //     const numberOne = document.querySelectorAll('.number');
+// //     const nOneText = numberOne.value;
+// //     console.log(nOneText);
+// // })
+
+
+
+
